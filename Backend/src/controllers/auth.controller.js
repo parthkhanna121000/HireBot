c7f0c55933cb0635a5669b5ef2db216a985f7598
parthkhanna121000 <khannaparth846@gmail.com>
2026-04-30 17:32:21 +0530

    Fix cross-domain cookie - set sameSite none and secure true

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -315,7 +315,7 @@ function setCookieAndRespond(res, statusCode, user, message) {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: true,
     sameSite: "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
